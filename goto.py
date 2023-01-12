88175aeeb9618e3e96cd4ba7e44754d8d6216b70
Benoit Schillings <benoit.schillings@gmail.com>
2023-01-11 20:58:29 -0800

    ibla

--- goto.py
+++ goto.py
@@ -26,17 +26,24 @@ except:
 
 
 from astroquery.vizier import Vizier
-from astropy.io.votable import from_table, writeto
+from astropy.io.votable import from_table, writeto, parse
 
 Vizier.ROW_LIMIT = -1
 
 catalog_list = Vizier.get_catalogs('VII/118/ngc2000')
-
 cat = catalog_list[0]
 
+import pickle
 
-writeto(cat, "ngc_cat.data", tabledata_format="binary")
+pickle.dump(cat, open('ngc.cat', 'wb'))
+ff = pickle.load(open('ngc.cat', 'rb'))
 
+#print(ff)
+
+idx = np.where(cat['Name'] == '100')
+ngc100_rows = cat[idx]
+
+print(ngc100_rows)
 if (sky == None):
 	print("Please start theSKY TCP Server")
 else:
